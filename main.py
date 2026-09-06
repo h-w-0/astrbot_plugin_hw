943cd1ca040b03964c59c00287d6d1fcce7d1d9f
h-w-0 <2745414403@qq.com>
2026-09-06 20:36:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from astrbot.api import logger, AstrBotConfig
 import astrbot.api.message_components as Comp
 import aiohttp
 
-API_URL = "https://hw233.cn/api/RandomImg.php"
+API_URL = "https://hw233.cn/api/RandomOC.php"
 
 
 @register(
